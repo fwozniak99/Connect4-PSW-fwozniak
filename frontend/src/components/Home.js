e5d4c676579c03caaed7a77ed00acb467ff93991
filip wozniak <filip.wozniak99@gmail.com>
2022-01-26 18:32:32 +0100

    added button creating a new game + styling for main page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import mqtt from 'mqtt';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import './Home.scss';
 const port = 8080;
+
 
 function Home() {
     const [ games, setGames ] = useState([]);
@@ -36,16 +40,26 @@ function Home() {
         }).catch(err => console.log(err));
     }
 
+    const createNewGame = () => {
+        axios.post(`http://localhost:${port}/games`)
+        .catch(error => console.log(error));
+    }
+
     return (
         <div>
-            CONNECT 4 ROOMS
-            {games && games.map(game => {
-                return (
-                <div key={game.id}>
-                    <Link to={`/games/${game.id}`}>{game.id}</Link>
-                </div>
+            <h2>CONNECT4 GAME</h2>
+            <Button onClick={() => {createNewGame()}} variant="contained" startIcon={<AddCircleOutlineIcon/>}>Create a new game</Button>
+            <h3 id="gamesTypography">List of games</h3>
+            <div className="listContainer">
+                {games && games.map(game => {
+                    return (
+                    <div key={game.id} className="roomContainer">
+                        <Link to={`/games/${game.id}`}>ENTER GAME</Link>
+                        <p>id: {game.id}</p>
+                    </div>
+                    )}
                 )}
-            )}
+            </div>
         </div>
     )
 }
